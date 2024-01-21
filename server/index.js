56e119ff031698b1e0835c02ae304be2e5ba7540
tracify <tracify@email.com>
2024-01-21 08:59:09 +0530

    tracify

---
+++ server/index.js
@@ -0,0 +1,30 @@
+// const express = require("express");
+
+// const app = express()
+
+// app.get(
+//     "/",
+//     (req,res) => {
+//         res.send("Hello")
+//     }
+// )
+
+// app.listen(
+//     5000,
+//     () => console.log("Backend is running")
+// )
+
+// const mongoose = require("mongoose");
+// // import UserModel from "./models/User.js";
+// mongoose
+//   .connect(
+//     "mongodb+srv://dev6589loper:3VdkfE2tt21vJWC2eEt@cluster0.roluyn6.mongodb.net/?retryWrites=true&w=majority"
+//   )
+//   .then((resolve) => {
+//     console.log("Connection done");
+//   })
+//   .catch((reject) => {
+//     console.log("Unable to connect");
+//   });
+// // console.log("Hello");
+// // user
